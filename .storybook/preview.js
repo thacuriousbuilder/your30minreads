340fe60002c208b65883f01a14a3c05cbef7d622
Alhouseny Camara <alhousenycamara@Matts-MacBook-Pro-2.local>
2022-12-03 22:23:19 -0500

    build: added tailwind and also added a lib to support storybook to work with tailwind

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import * as NextImage from 'next/image';
+import 'tailwindcss/tailwind.css';
 
 const BREAKPOINTS_INT = {
   xs: 375,
--- components/template/base/BaseTemplate.tsx
+++ components/template/base/BaseTemplate.tsx
@@ -3,7 +3,7 @@ export interface IBaseTemplate {
   }
   
   const BaseTemplate: React.FC<IBaseTemplate> = ({ sampleTextProp }) => {
-    return <div >{sampleTextProp}</div>;
+    return <div className="bg-gradient-to-r from-cyan-500 to-blue-500" >{sampleTextProp}</div>;
   };
   
   export default BaseTemplate;
