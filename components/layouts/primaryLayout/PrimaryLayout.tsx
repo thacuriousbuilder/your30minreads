1c36a57f9dd2b9021b33d0b1825dcb8a3eb05ded
Alhouseny Camara <alhousenycamara@Matts-MacBook-Pro-2.local>
2022-12-12 15:45:53 -0500

    feat: added a primary layout that takes the header and footer

---
+++ components/layouts/primaryLayout/PrimaryLayout.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import Footer from '../../navigation/footer/Footer';
+import Header from '../../navigation/header/Header';
+
+export interface IPrimaryLayout extends React.ComponentPropsWithoutRef<'div'> {
+  justify?: 'items-center' | 'items-start';
+}
+  
+  const PrimaryLayout: React.FC<IPrimaryLayout> = ({ justify,children,  ...divProps }) => {
+    return (
+      <>
+        <Head>
+          <title>NextJs Fullstack App Template</title>
+        </Head>
+        <div {...divProps} className={`min-h-screen flex flex-col ${justify}`}>
+          <Header />
+          <main className="px-5">{children}</main>
+          <div className="m-auto" />
+          <Footer developerName='Alhouseny Camara' yearOfUpdate={2022} />
+        </div>
+      </>
+    );
+  };
+  
+  export default PrimaryLayout;
