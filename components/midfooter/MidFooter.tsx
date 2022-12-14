0cfa10de19870d0718c9e173a5572d7437dd134e
Alhouseny Camara <alhousenycamara@Matts-MacBook-Pro-2.local>
2022-12-13 20:59:54 -0500

    feat: added the midfooter moved it into primaryLayout and creadted a secondaryLayout

--- components/midfooter/MidFooter.tsx
+++ components/midfooter/MidFooter.tsx
@@ -1,14 +1,19 @@
+import Image from "next/image";
 export interface IMidFooter {}
   
 
   // eslint-disable-next-line no-empty-pattern
   const MidFooter: React.FC<IMidFooter> = ({ }) => {
     return(
-          <div className="flex flex-row bg-color-400 h-full w-full">
-       <span className="bg-gray-300 h-52 w-52 rounded-sm space-x-5 m5"></span>
-       <span className="bg-gray-300 h-52 w-52  rounded-sm"></span>
-       <span className="bg-gray-300 h-52 w-52  rounded-sm"></span>
-          </div>
+         
+            <footer className="">
+              <div className="flex flex-row  bg-color-400  p-10 justify-between">
+                       <div className="h-96 w-72 bg-gray-200 ml-20"></div>
+                       <div className="h-96 w-72 bg-gray-200"></div>
+                       <div className="h-96 w-72 bg-gray-200 mr-20"></div>
+              </div>
+            </footer>
+          
 
     )
   };
