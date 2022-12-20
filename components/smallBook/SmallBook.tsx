58ce6ba0b9a5e555b4808daeef8e1dcbd4ca919f
Alhouseny Camara <alhousenycamara@Matts-MacBook-Pro-2.local>
2022-12-19 21:39:36 -0500

    feat: testing out how all the components needed for the landingpage will look altogether

--- components/smallBook/SmallBook.tsx
+++ components/smallBook/SmallBook.tsx
@@ -11,17 +11,17 @@ export interface ISmallBook {
     const returnRating=(rating: any)=>{
       const stars= []
       for(let i=0; i< rating; i++){
-       stars.push(<span key={i}><AiFillStar size={10}/></span>)
+       stars.push(<span key={i}><AiFillStar size={15}/></span>)
       }
       return stars
     }
     return(
       <div>
-              <Image src={image} alt="cover image for books" width={100} height={100}/>
+              <Image className="rounded-sm" src={image} alt="cover image for books" width={100} height={100}/>
         <div className="flex flex-col list-none justify-center">
             <li className="font-bold">{title}</li>
-            <li className="text-sm">{author}</li>
-            <li className="flex flex-row">{returnRating(rating)}</li>
+            <li className="text-sm font-semibold ">{author}</li>
+            <li className="flex flex-row ">{returnRating(rating)}</li>
         </div>
       </div>
     )
