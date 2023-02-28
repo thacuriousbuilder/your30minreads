3d65a33e1054413e1e8b93a89de93a91bb8a5555
Alhouseny Camara <camaraalhouseny44@gmail.com>
2023-02-27 19:08:21 -0500

    pulling data from graphql testing

--- components/layouts/primaryLayout/PrimaryLayout.tsx
+++ components/layouts/primaryLayout/PrimaryLayout.tsx
@@ -7,50 +7,12 @@ import HeaderBanner from '../../headerBanner/HeaderBanner';
 import ModalPopup from '../../modalPopUp/ModalPopUp';
 import OverlayContent from '../../overlayContent/OverlayContent';
 import { useEffect, useState } from 'react';
-import { GetStaticProps } from 'next';
-import { CONST_CONFIG } from '../../../constants/config';
-
 
 
 export interface IPrimaryLayout extends React.ComponentPropsWithoutRef<'div'> {
   justify?: 'items-center' | 'items-start';
 }
-
-export const getStaticProps: GetStaticProps = async () => {
-  const fetchParams ={
-    method:'post',
-    headers:{
-      'content-type':'application/json'
-    },
-    body: JSON.stringify({
-      query:`{
-       newbooks{
-        data{
-          attributes{
-            image{
-            data{
-              attributes{
-                url
-              }
-            }
-            }
-            titleText
-          }
-        }
-      }
-    }`
-    })
-  }
-  const res = await fetch(`${CONST_CONFIG.BASE_URL}graphql`,fetchParams)
-  console.log(res)
-  const data = await res.json()
-  console.log(data)
-  return {
-    props: data,
-  };
-}
-  
-  const PrimaryLayout: React.FC<IPrimaryLayout> = ({ justify,children,  ...divProps },) => {
+ const PrimaryLayout: React.FC<IPrimaryLayout> = ({ justify,children,  ...divProps },) => {
     const [isOpen, setIsOpen] = useState(false);
     // console.log("book:",data)
     //shows the modal once countdown is done.
